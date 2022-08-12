c6fa1b40023eed2cf79591219d00d62fee28ca30
Djonatan Bonelli <100157600+FenrirTG1@users.noreply.github.com>
2022-08-12 15:02:19 -0300

    Update NerdFlix.py

--- NerdFlix.py
+++ NerdFlix.py
@@ -1,15 +1,17 @@
 from datetime import date, datetime
-import pandas as pd
+
+#PROJETO NERDFLIX
 
 #produtos
 class product:
-    def __init__(self, nome, codigo, preco, tipo, disponivel):
+    def __init__(self, nome, codigo, preco, tipo, disponivel ):
         self.nome = nome
         self.codigo = codigo
         self.preco = preco
         self.tipo = tipo
         self.disponivel = disponivel
 
+#listas para utilidades
 detalhes = []
 codigo_comprado = []
 nome_comprado = []
@@ -29,11 +31,14 @@ def verificacao():
             verificar = input("Formato inválido, tente novamente: ")
 
     for i in range(len(detalhes)):
+        verificar = str(verificar)
         if detalhes[i].codigo == verificar:
             print(f"{detalhes[i].codigo}, {detalhes[i].nome}, R${detalhes[i].preco}, {detalhes[i].tipo}, {detalhes[i].disponivel}")
-
-        while detalhes[i].codigo != verificar:
-            verificar = input("Produto inexistente, tente novamente: ")
+        
+        verificar = int(verificar)
+        if i == verificar:
+            while detalhes[i].codigo != verificar:
+                verificar = input("Produto inexistente, tente novamente: ")
     iniciar()
 #cadastrar produtos
 def cadastro():
@@ -47,6 +52,7 @@ def cadastro():
         while True:    
             try:
                 product.preco = float(product.preco)
+                product.preco > 0
                 break
             except:
                 product.preco = input("Formato inválido, digite o preço novamente: ")
@@ -58,10 +64,10 @@ def cadastro():
 
         product.disponivel = input("Disponível(s/n): ")
         while product.disponivel != "s" and product.disponivel != "n":
-            product.tipo = input("Formato inválido, digite novamente(s/n): ")
+            product.disponivel = input("Formato inválido, digite novamente(s/n): ")
 
+#tratamento de strings
         product.codigo += 1
-
         if product.tipo == 1:
             product.tipo = "Série"
         elif product.tipo == 2:
@@ -72,7 +78,9 @@ def cadastro():
             product.disponivel = "Disponível"
         elif product.disponivel == "n":
             product.disponivel = "Indisponível"
-        detail = product(product.nome, product.codigo, product.preco, product.tipo, product.disponivel)
+
+#detail recebe a classe e detalhes adiciona os parâmetros na lista
+        detail = product(product.nome, str(product.codigo), product.preco, product.tipo, product.disponivel)
         detalhes.append(detail)
         
         print(f"{detail.codigo}, {detail.nome}, R${detail.preco}, {detail.tipo}, {detail.disponivel}")
@@ -89,18 +97,22 @@ def atualizar():
         except:
             verificar = input("Formato inválido, tente novamente: ")
 
+
+        #verificação de existência       
     for i in range(len(detalhes)):
         if detalhes[i].codigo == verificar:
             print(f"{detalhes[i].codigo}, {detalhes[i].nome}, R${detalhes[i].preco}, {detalhes[i].tipo}, {detalhes[i].disponivel}")
+        if i == verificar:
+            while detalhes[i].codigo != verificar:
+                verificar = input("Produto inexistente, tente novamente: ")
 
-        while detalhes[i].codigo != verificar:
-            verificar = input("Produto inexistente, tente novamente: ")
-
+#método de atualização
     print("1 - Atualizar nome\n2 - Atualizar preço\n3 - Atualizar Tipo\n4 - Atualizar disponibilidade")
-    metodat = input("Método de atualização: ")
-    while metodat != "1" and metodat != "2" and metodat != "3" and metodat != "4": 
-        metodat = input("Método inválido, digite novamente: ")
+    methodat = input("Método de atualização: ")
+    while methodat != "1" and methodat != "2" and methodat != "3" and methodat != "4": 
+        methodat = input("Método inválido, digite novamente: ")
 
+#confirmar
     confirmacao = input("\nConfirmação(s/n): ")
     if confirmacao != "s" and confirmacao != "n":
         confirmacao = input("Formato inválido, digite a confirmação novamente: ")
@@ -111,17 +123,19 @@ def atualizar():
     while confirmacao != 's' and confirmacao != 'n':
         confirmacao = input("Método inválido, digite novamente: ")
 
-    if metodat == "1" and confirmacao == "s":
+    if methodat == "1" and confirmacao == "s":
         product.nome = input("\nNome: ")
 
         for i in range(len(detalhes)):
             detalhes[i].nome = product.nome
          
-    elif metodat == "2" and confirmacao == "s":
+    elif methodat == "2" and confirmacao == "s":
         product.preco = input("Preço: ")
+        
         while True:    
             try:
                 product.preco = float(product.preco)
+                product.preco > 0
                 break
             except:
                 product.preco = input("Formato inválido, digite o preço novamente: ")
@@ -129,7 +143,7 @@ def atualizar():
         for i in range(len(detalhes)):
             detalhes[i].preco = product.preco
 
-    elif metodat == "3" and confirmacao == "s":
+    elif methodat == "3" and confirmacao == "s":
         product.tipo = input("Tipo: ")
 
         while product.tipo != "1" and product.tipo != "2" and product.tipo != "3":
@@ -138,7 +152,7 @@ def atualizar():
         for i in range(len(detalhes)):
             detalhes[i].tipo = product.tipo
 
-    elif metodat == "4" and confirmacao == "s":
+    elif methodat == "4" and confirmacao == "s":
         product.disponivel = input("Disponibilidade(s/n): ")
 
         while product.disponivel != "s" and product.disponivel != "n":
@@ -149,7 +163,6 @@ def atualizar():
         elif product.disponivel == "n":
             product.disponivel = "Indisponível"
         
-
         for i in range(len(detalhes)):
             detalhes[i].disponivel = product.disponivel
     iniciar()
@@ -157,53 +170,64 @@ def atualizar():
 #registra compra
 def registrar_compra():
     global login
-  ##  compra = []
     login = input("Insira seu login: ")
     while login == '':
         login = input("Insira seu login: ")
-
     while True:          
         cod_conf = input("Digite o código do produto: ")
-        for i in range(len(detalhes)):
-            while i+1 != detalhes[i].codigo:
-                cod_conf = input("Código inválido, tente novamente: ")
         if cod_conf == '':
             break
-        print(cod_conf)
-        print("1 - Confirmar\n2 - Cancelar")
-        confirmar_compra = input("Método: ")
-        while confirmar_compra != '1' and confirmar_compra != '2':
-            confirmar_compra = input("Método inválido, digite novamente: ") 
-        if confirmar_compra == '1':
-            codigo_comprado.append(cod_conf)
-        else:
-            cod_conf = input("Digite o código do produto: ")
+        for i in range(len(detalhes)):
+            cod_conf = int(cod_conf)
+            if i+1 == cod_conf:
+                while detalhes[i].disponivel == "Indisponível":
+                    cod_conf = input("Código inválido, tente novamente: ")
+                    if cod_conf == '':
+                        break
+#verificação de existência
+        for i in range(len(detalhes)):
+            cod_conf = str(cod_conf)
+            if cod_conf == detalhes[i].codigo:
+                print(cod_conf)
+                print("\n1 - Confirmar\n2 - Cancelar")
+                confirmar_compra = input("Método: ")
+                while confirmar_compra != '1' and confirmar_compra != '2':
+                    confirmar_compra = input("Método inválido, digite novamente: ") 
+#adiciona os elementos em suas respectivas listas para impressão
+                codigo_comprado.append(cod_conf)
+                nome_comprado.append(detalhes[i].nome)
+                preco_comprado.append(detalhes[i].preco)
+                tipo_comprado.append(detalhes[i].tipo)
+                print(f"Comprado:{codigo_comprado} {nome_comprado} R${preco_comprado} {tipo_comprado}\n")
+                break
         time()
     iniciar()
 
+#relatório de produtos
 def relatorio_produtos():
-    print("0 - Todos os produtos\n1 - Somente filmes\n2 - Séries\n3 - Documentários\n4 - Todos os produtos disponíveis\n5 - Todos os produtos indisponíveis")
+    print("\n0 - Todos os produtos\n1 - Somente filmes\n2 - Séries\n3 - Documentários\n4 - Todos os produtos disponíveis\n5 - Todos os produtos indisponíveis")
     opcao = input("\nFiltro: ")
     while opcao != "0" and opcao != "1" and opcao != "2" and opcao != "3" and opcao != "4" and opcao != "5":
         opcao = input("Filtro inválido, tente novamente: ")
  
+ #métodos e filtros
     if opcao == '0':
         for i in range(len(detalhes)):
             print(f"{detalhes[i].codigo}, {detalhes[i].nome}, R${detalhes[i].preco}, {detalhes[i].tipo}, {detalhes[i].disponivel}")
             
     elif opcao == '1':
         for i in range(len(detalhes)):
-            if detalhes[i].tipo == 2:
+            if detalhes[i].tipo == "Filme":
                 print(f"{detalhes[i].codigo}, {detalhes[i].nome}, R${detalhes[i].preco}, {detalhes[i].tipo}, {detalhes[i].disponivel}")
 
     elif opcao == '2':
         for i in range(len(detalhes)):
-            if detalhes[i].tipo == 1:
+            if detalhes[i].tipo == "Série":
                 print(f"{detalhes[i].codigo}, {detalhes[i].nome}, R${detalhes[i].preco}, {detalhes[i].tipo}, {detalhes[i].disponivel}")
             
     elif opcao == '3':
         for i in range(len(detalhes)):
-            if detalhes[i].tipo == 3:
+            if detalhes[i].tipo == "Documentário":
                 print(f"{detalhes[i].codigo}, {detalhes[i].nome}, R${detalhes[i].preco}, {detalhes[i].tipo}, {detalhes[i].disponivel}")
 
     elif opcao == '4':
@@ -218,25 +242,17 @@ def relatorio_produtos():
     
     print("\nA consulta foi concluída")
     iniciar()
-    
+
+#relatório de compras
 def relatorio_compras():
 
     print(login, data)
-    for x in range(len(detalhes)):
-        for i in codigo_comprado:
-            if detalhes[x].codigo == i:
-                nome_comprado.append(detalhes[i].nome)
-                preco_comprado.append(detalhes[i].preco)
-                tipo_comprado.append(detalhes[i].tipo)
-        
-    tabelas = {'Código': [codigo_comprado],
-    'Nome': [nome_comprado],
-    'Preço': [preco_comprado],
-    'Tipo': [tipo_comprado]}
+    print("\nCódigo  Nome  Preço  Tipo")
+    for i in range(len(codigo_comprado)):
+        print(f"{codigo_comprado[i]}, {nome_comprado[i]}, R${preco_comprado[i]}, {tipo_comprado[i]}")
+            
     
-    tabela = pd.DataFrame(tabelas)
-    
-    print(tabela)
+    iniciar()
 
 #data e hora:
 def time():
@@ -246,15 +262,14 @@ def time():
     print(data)
 
 #inicia o programa:
-
 def iniciar():
 
-    #mensagem inicial e verificação de códigox' 
+    #mensagem inicial e encaminhamento de funções 
 
-    print("1 - Cadastrar\n2 - Consultar\n3 - Atualizar\n4 - Registrar Compra\n5 - Relatório de produtos\n6 - Relatório de compras")
+    print("\n1 - Cadastrar\n2 - Consultar\n3 - Atualizar\n4 - Registrar Compra\n5 - Relatório de produtos\n6 - Relatório de compras")
     method = input("\nDigite sua ação: ")
     while method != '1' and method != '2' and method != '3' and method != '4' and method != '5' and method != '6':
-        method = input("\nFormato inválido, digite sua ação novamente: ")
+        method = input("\nMétodo inválido, digite sua ação novamente: ")
 
     if method == "1":
         cadastro()
